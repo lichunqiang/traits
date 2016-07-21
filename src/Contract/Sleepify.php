1613341477c3b5e112064688b4b7c4c41c7fcfbb
lichunqiang <light-li@hotmail.com>
2016-07-21 10:49:20 +0800

    initialize && clean

--- src/Contract/Sleepify.php
+++ src/Contract/Sleepify.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the light/traits.
+ *
+ * (c) lichunqiang <light-li@hotmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Light\Traits\Contract;
 
 use ReflectionClass;
@@ -12,6 +21,7 @@ trait Sleepify
     public function __sleep()
     {
         $properties = (new ReflectionClass($this))->getProperties();
+
         return array_map(function ($property) {
             return $property->getName();
         }, $properties);
@@ -40,6 +50,7 @@ trait Sleepify
     protected function getPropertyValue(\ReflectionProperty $property)
     {
         $property->setAccessible(true);
+
         return $property->getValue($this);
     }
 }
--- src/DesignPattern/Instance.php
+++ src/DesignPattern/Instance.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the light/traits.
+ *
+ * (c) lichunqiang <light-li@hotmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Light\Traits\DesignPattern;
 
 /**
